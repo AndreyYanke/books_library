d87a38d66f229e258dcbf21d485c68c5a45f9c5d
geekbrains <ayankeav@gmail.com>
2022-02-14 00:20:49 +0300

    добавил отражение в шаблоне и убрал закомментированные строки

--- booksapp/views.py
+++ booksapp/views.py
@@ -8,21 +8,8 @@ from booksapp.models import Book
 from booksapp.serializers import BooksModelSerializer
 
 
-# from booksapp.serializers import (
-#     BooksListSerializer,
-#     BooksDetailSerializer,
-#     BooksCreateSerializer,
-#     BooksUpdateSerializer,
-#     BooksDeleteSerializer,
-#     AuthorListSerializer,
-#     AuthorDetailSerializer,
-#     GenreListSerializer,
-#     GenreDetailSerializer,
-# )
-
-
 class BooksModelViewSet(ModelViewSet):
-    queryset = Book.objects.all()
+    queryset = Book.objects.prefetch_related('authors').all()
     serializer_class = BooksModelSerializer
 
     filter_backends = (DjangoFilterBackend,)
@@ -32,61 +19,3 @@ class BooksModelViewSet(ModelViewSet):
         self.queryset = self.filter_queryset(self.get_queryset())
         context = {'filter': BookFilter(self.request.GET, queryset=self.get_queryset())}
         return Response(context, template_name='booksapp/books.html')
-
-
-
-
-# class BooksListView(generics.ListAPIView):
-#     """Вывод списка книг"""
-#     queryset = Book.objects.all()
-#     serializer_class = BooksListSerializer
-#
-#
-# class BooksDetailView(generics.RetrieveAPIView):
-#     """Вывод полного списка книг с описанием, авторами и жанрами"""
-#     queryset = Book.objects.all()
-#     serializer_class = BooksDetailSerializer
-#
-#
-#
-#
-# class BooksCreateView(generics.CreateAPIView):
-#     """Создание книг"""
-#     queryset = Book.objects.all()
-#     serializer_class = BooksCreateSerializer
-#
-#
-# class BooksUpdateView(generics.RetrieveUpdateAPIView):
-#     """Редактирование созданной книг"""
-#     queryset = Book.objects.all()
-#     serializer_class = BooksUpdateSerializer
-#
-#
-# class BooksDeleteView(generics.RetrieveDestroyAPIView):
-#     """Удаление созданной книг"""
-#     queryset = Book.objects.all()
-#     serializer_class = BooksDeleteSerializer
-#
-#
-# class AuthorListView(generics.ListAPIView):
-#     """Вывод авторов"""
-#     queryset = Author.objects.all()
-#     serializer_class = AuthorListSerializer
-#
-#
-# class AuthorDetailView(generics.RetrieveAPIView):
-#     """Вывод авторов"""
-#     queryset = Author.objects.all()
-#     serializer_class = AuthorDetailSerializer
-#
-#
-# class GenreListView(generics.ListAPIView):
-#     """Вывод авторов"""
-#     queryset = Genre.objects.all()
-#     serializer_class = GenreListSerializer
-#
-#
-# class GenreDetailView(generics.RetrieveAPIView):
-#     """Вывод авторов"""
-#     queryset = Genre.objects.all()
-#     serializer_class = GenreDetailSerializer
